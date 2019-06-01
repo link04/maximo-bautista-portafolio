9828b3ebec2167cfd3f2ff19b8d72ee3f58d416e
Maximo Bautista <max.bautista97@gmail.com>
2019-06-01 00:30:13 -0400

    Updates

---
+++ precache-manifest.910a7931afd4ba57fa6fe5be1ea5a926.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d751233c0ff1f835aa57b2590b77bb16",
+    "url": "/maximo-bautista-portafolio/index.html"
+  },
+  {
+    "revision": "ef3b9e841ae47e246a6a",
+    "url": "/maximo-bautista-portafolio/static/css/main.16f562a3.chunk.css"
+  },
+  {
+    "revision": "6cbf32e6e7458f998581",
+    "url": "/maximo-bautista-portafolio/static/js/2.5646c6a5.chunk.js"
+  },
+  {
+    "revision": "ef3b9e841ae47e246a6a",
+    "url": "/maximo-bautista-portafolio/static/js/main.93a126bc.chunk.js"
+  },
+  {
+    "revision": "d9d3aa583cc9c93303c4",
+    "url": "/maximo-bautista-portafolio/static/js/runtime~main.7432ed6e.js"
+  },
+  {
+    "revision": "4d7eb9df5dbb7247581a075ec437ab2a",
+    "url": "/maximo-bautista-portafolio/static/media/coffee-shop-blogs.4d7eb9df.png"
+  },
+  {
+    "revision": "0408c35be5d85a79239ba8c19f8704fc",
+    "url": "/maximo-bautista-portafolio/static/media/craveme-screenshot.0408c35b.png"
+  },
+  {
+    "revision": "11f38c6e0006da5f075dc3d3bb7a537c",
+    "url": "/maximo-bautista-portafolio/static/media/js-tic-tac-toe.11f38c6e.png"
+  },
+  {
+    "revision": "c5f5ed37c66372146d6de22712891b6f",
+    "url": "/maximo-bautista-portafolio/static/media/mysql.c5f5ed37.svg"
+  },
+  {
+    "revision": "85af04121a61ffd8709f8a72c4b6c685",
+    "url": "/maximo-bautista-portafolio/static/media/postgresql.85af0412.svg"
+  },
+  {
+    "revision": "21fe4cae5ea6fdb4d9802de8ecb5be0f",
+    "url": "/maximo-bautista-portafolio/static/media/profile1.21fe4cae.jpg"
+  },
+  {
+    "revision": "989ba1382a0b1e0ee3bbdbd47b954784",
+    "url": "/maximo-bautista-portafolio/static/media/profile2.989ba138.jpg"
+  },
+  {
+    "revision": "0164c3a1e10cbe96b7a2b547c166d145",
+    "url": "/maximo-bautista-portafolio/static/media/rails.0164c3a1.svg"
+  },
+  {
+    "revision": "04bf1646e742803c4fb627768afd6e42",
+    "url": "/maximo-bautista-portafolio/static/media/redux.04bf1646.svg"
+  },
+  {
+    "revision": "d96960c86067f89b7dca4fb53c28e897",
+    "url": "/maximo-bautista-portafolio/static/media/ruby.d96960c8.svg"
+  },
+  {
+    "revision": "36462ab5be205694b53856716d0af548",
+    "url": "/maximo-bautista-portafolio/static/media/tom-pilgrim-vs-the-universe.36462ab5.png"
+  },
+  {
+    "revision": "561bad2c13523304f5118de6389c5d1e",
+    "url": "/maximo-bautista-portafolio/static/media/triforce.561bad2c.png"
+  },
+  {
+    "revision": "0ec54958686952b818c179ac57795183",
+    "url": "/maximo-bautista-portafolio/static/media/triforce.svg.0ec54958.png"
+  }
+]);
