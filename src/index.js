23cf2edf7ceed919a01c0d64e2dfb9e8994c4b0c
Maximo  Bautista <max.bautista97@gmail.com>
2020-06-03 18:46:36 -0400

    Added Datadog  Key For testing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import './assets/css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+window.DD_RUM && window.DD_RUM.init({
+  clientToken: 'pubb3ff4bcdd5af1c9c05f965d34edafdd9',
+  applicationId: '0234e0ff-65dd-4f50-a443-8aee8686d493',
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
